7bf4b88998c60ae9b0435c293996292587fff8ba
Vasiliy Girichev <girichev2013@yandex.ru>
2022-11-29 11:11:02 +0500

    29.11.2022 - ТЗ=9, rev1 - устранены замечания, проверка файлов

--- src/main/java/ru/yandex/practicum/filmorate/FilmorateApplication.java
+++ src/main/java/ru/yandex/practicum/filmorate/FilmorateApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class FilmorateApplication {
+    // Проверка гит
     public static void main(String[] args) {
         SpringApplication.run(FilmorateApplication.class, args);
     }
