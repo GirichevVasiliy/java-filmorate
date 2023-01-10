331032559f398f8429bbd87f2547495a98a704da
Vasiliy Girichev <girichev2013@yandex.ru>
2023-01-10 18:43:33 +0500

    10.01.2022 - Mpa добавил

---
+++ src/main/java/ru/yandex/practicum/filmorate/storage/like/LikeDbStorage.java
@@ -0,0 +1,4 @@
+package ru.yandex.practicum.filmorate.storage.like;
+
+public interface LikeDbStorage {
+}
---
+++ src/main/java/ru/yandex/practicum/filmorate/storage/mpa/impl/MpaDbStorage.java
@@ -0,0 +1,30 @@
+package ru.yandex.practicum.filmorate.storage.mpa.impl;
+
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+import ru.yandex.practicum.filmorate.model.MPA;
+import ru.yandex.practicum.filmorate.storage.mpa.MpaStorage;
+
+import java.util.Collection;
+
+@Repository
+public class MpaDbStorage implements MpaStorage {
+    private final JdbcTemplate jdbcTemplate;
+
+    public MpaDbStorage(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    @Override
+    public Collection<MPA> getAll() {
+        return jdbcTemplate.query("SELECT * FROM MPA_RATING", new BeanPropertyRowMapper<>(MPA.class));
+    }
+
+    @Override
+    public MPA getById(Integer id) {
+        return jdbcTemplate.query("SELECT * FROM MPA_RATING WHERE id=?", new Object[]{id},
+                        new BeanPropertyRowMapper<>(MPA.class))
+                .stream().findAny().orElse(null);
+    }
+}
