d2faa276d6eb7e8a8e1156e64847e6c9fb8ed420
Vasiliy Girichev <girichev2013@yandex.ru>
2023-01-11 17:52:58 +0500

    11.01.2022 - Тестирование

--- src/main/java/ru/yandex/practicum/filmorate/controller/MpaController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/MpaController.java
@@ -1,17 +1,33 @@
 package ru.yandex.practicum.filmorate.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import ru.yandex.practicum.filmorate.model.MPA;
 import ru.yandex.practicum.filmorate.service.MpaService;
+
+import java.util.Collection;
 
 @RestController
 @RequestMapping("/mpa")
 
 public class MpaController {
     private final MpaService mpaService;
-@Autowired
+
+    @Autowired
     public MpaController(MpaService mpaService) {
         this.mpaService = mpaService;
+    }
+
+    @GetMapping
+    public Collection<MPA> getAll() {
+        return mpaService.getAll();
+    }
+
+    @GetMapping("/{id}")
+    public MPA getById(@PathVariable Integer id) {
+        return mpaService.getById(id);
     }
 }
